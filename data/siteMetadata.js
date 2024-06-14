fd0ae6461234553981a1c6cae5b09f83fd1c24c4
Louis Baker <d.lee77@icloud.com>
2024-06-14 15:11:27 +0800

    修改网站

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,10 +1,10 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: '路易笔记',
+  author: 'Louis Baker',
+  headerTitle: '路易笔记',
+  description: '一个全栈工程师用来记录工作学习中的一些知识点, 记录和分享日常学习技术内容的笔记.',
+  language: 'zh-CN',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
