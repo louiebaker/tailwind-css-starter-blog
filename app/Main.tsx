f432fec7fbdc452a6c41985b9b71da3a15379f14
Louis Baker <d.lee77@icloud.com>
2024-06-15 15:50:37 +0800

    111

--- app/Main.tsx
+++ app/Main.tsx
@@ -67,17 +67,7 @@ export default function Home({ posts }) {
           })}
         </ul>
       </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-            aria-label="All posts"
-          >
-            All Posts &rarr;
-          </Link>
-        </div>
-      )}
+
       {/* {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
