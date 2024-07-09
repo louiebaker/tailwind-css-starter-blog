731e8ae08d37435236dfd8127b5488344d715c48
Louis Baker <d.lee77@icloud.com>
2024-07-09 15:09:30 +0800

    添加公安备案

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,26 @@
-import Link from './Link'
+import Image from 'next/image'
 
 export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <a href="http://beian.miit.gov.cn/" target="_blank">
+        <div className="mb-8 flex flex-col items-center gap-4 text-sm text-gray-500 dark:text-gray-400  sm:flex-row">
+          <a
+            href="http://www.beian.gov.cn/portal/registerSystemInfo"
+            target="_blank"
+            className="flex items-center gap-0.5"
+          >
+            <Image
+              src="/static/images/police-badge.png"
+              alt=""
+              className="inline-block"
+              width={16}
+              height={17}
+            />
+            <span className="hover:text-primary-500">豫公网安备 41022502000131号</span>
+          </a>
+
+          <a href="http://beian.miit.gov.cn/" target="_blank" className="hover:text-primary-500">
             豫ICP备2024067599号-1
           </a>
         </div>
